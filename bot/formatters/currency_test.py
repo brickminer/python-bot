a2a6f3a02e84a4f72bf15028846ecfe8b962e7ba
Luis Nascimento <luis.nascimento@taotesting.com>
2020-05-05 16:34:44 +0200

    Fixing unit test error

--- bot/formatters/currency_test.py
+++ bot/formatters/currency_test.py
@@ -5,6 +5,6 @@ from bot.formatters import currency
 def test_format():
     assert currency.format(None) == 'N/A'
     assert currency.format('0.00', 'USD') == 'N/A'
-    assert currency.format(5, 'USD') == '$6.00'
+    assert currency.format(5, 'USD') == '$5.00'
     assert currency.format(5, 'EUR') == '€5.00'
     assert currency.format(5, 'GBP') == '£5.00'
